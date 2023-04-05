0dc191cc1032887a76eaf9f911755e0500fdd980
Roberto <roberto.henschel@lambda-loginnode01.cm.cluster>
2023-04-05 12:57:08 -0500

    reodered options

--- app_text_to_video.py
+++ app_text_to_video.py
@@ -53,14 +53,15 @@ def create_demo(model: Model):
                     else:
                         video_length = gr.Number(
                             label="Video length", value=8, precision=0)
-                    merging_ratio = gr.Slider(
-                        label="Merging ratio", minimum=0.0, maximum=0.9, step=0.1, value=0.0, visible=not on_huggingspace,
-                        info="Ratio of how many tokens are merged. The higher the more compression (less memory and faster inference)."
-                    )
-                    chunk_size = gr.Slider(
-                        label="Chunk size", minimum=2, maximum=16, value=12 if on_huggingspace else 8, step=1, visible=not on_huggingspace,
-                        info="Number of frames processed at once. Reduce for lower memory usage."
-                    )
+
+                    n_prompt = gr.Textbox(
+                        label="Optional Negative Prompt", value='')
+                    seed = gr.Slider(label='Seed',
+                                     info="-1 for random seed on each run. Otherwise, the seed will be fixed.",
+                                     minimum=-1,
+                                     maximum=65536,
+                                     value=0,
+                                     step=1)
 
                     motion_field_strength_x = gr.Slider(
                         label='Global Translation $\\delta_{x}$', minimum=-20, maximum=20,
@@ -79,14 +80,14 @@ def create_demo(model: Model):
                                    info="Perform DDPM steps from t0 to t1. The larger the gap between t0 and t1, the more variance between the frames.",
                                    maximum=48, value=47, step=1)
 
-                    n_prompt = gr.Textbox(
-                        label="Optional Negative Prompt", value='')
-                    seed = gr.Slider(label='Seed',
-                                     info="-1 for random seed on each run. Otherwise, the seed will be fixed.",
-                                     minimum=-1,
-                                     maximum=65536,
-                                     value=0,
-                                     step=1)
+                    merging_ratio = gr.Slider(
+                        label="Merging ratio", minimum=0.0, maximum=0.9, step=0.1, value=0.0, visible=not on_huggingspace,
+                        info="Ratio of how many tokens are merged. The higher the more compression (less memory and faster inference)."
+                    )
+                    chunk_size = gr.Slider(
+                        label="Chunk size", minimum=2, maximum=16, value=12 if on_huggingspace else 8, step=1, visible=not on_huggingspace,
+                        info="Number of frames processed at once. Reduce for lower memory usage."
+                    )
             with gr.Column():
                 result = gr.Video(label="Generated Video")
 
